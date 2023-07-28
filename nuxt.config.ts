69c9a2525712c9cf932bc2353d30c3f30fbf4bf1
Matthew Trotman <savageguy@gmail.com>
2023-07-28 22:35:51 +1200

    Update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
-import { defineNuxtConfig } from 'nuxt';
-
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt', '@nuxtjs/color-mode', 'trpc-nuxt', '@vueuse/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt', '@nuxtjs/color-mode', '@vueuse/nuxt'],
   runtimeConfig: {
     public: {
       baseUrl: 'https://localhost:3000',
@@ -13,14 +11,9 @@ export default defineNuxtConfig({
     configPath: 'tailwind.config.ts',
     exposeConfig: false,
     injectPosition: 0,
-    viewer: true,
   },
   formkit: {
     configFile: 'formkit.config.js',
-  },
-  trpc: {
-    baseURL: process.env.NUXT_PUBLIC_BASE_URL,
-    endpoint: '/trpc',
   },
   typescript: {
     strict: true,
@@ -29,4 +22,7 @@ export default defineNuxtConfig({
     dataValue: 'theme',
     classSuffix: '',
   },
+  build: {
+    transpile: ['trpc-nuxt']
+  }
 });
